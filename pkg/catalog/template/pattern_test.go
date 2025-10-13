c80a18c4cbfadebd509007b330c278009a43d564
Groboclown <groboclown@gmail.com>
2025-10-12 23:36:24 -0500

    Fix test.

--- pkg/catalog/template/pattern_test.go
+++ pkg/catalog/template/pattern_test.go
@@ -3,7 +3,7 @@ package template_test
 import (
 	"testing"
 
-	"github.com/groboclown/vex-catalog-go/pkg/template"
+	"github.com/groboclown/vex-catalog-go/pkg/catalog/template"
 	"github.com/package-url/packageurl-go"
 )
 
